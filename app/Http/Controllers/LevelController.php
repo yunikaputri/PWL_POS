a25189f267cd7247801a8285997b0493bba9a635
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-09-13 11:52:13 +0700

    Jobsheet 4_Praktikum 4

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+    public function index()
+    {
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // return 'Insert data baru berhasil';
+
+        //UPDATE
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+
+        //HAPUS
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
+
+        //MENAMPILKAN
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
+    }
+}
