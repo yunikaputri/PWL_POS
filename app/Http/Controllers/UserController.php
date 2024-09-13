afdc463db80906ac029151ebd9ad5596677f1fe2
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-09-13 15:06:20 +0700

    Jobsheet 3_Praktikum 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    
+    public function index()
+    {
+        //TAMBAH
+        /*DB::insert('insert into m_level (level_id, level_kode, level_nama, created_at) values(?, ?, ?, ?)', ['4', 'CUS', 'Customer', now()]);
+        return 'Insert data baru berhasil'; */
+
+        // Tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //update data user
+
+        
+        // UserModel::insert($data); // Tambahkan data ke tabel m_user
+
+        // Coba akses model UserModel
+        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
+
+    // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $table = 'm_user';
+
+    // Mendefinisikan primary key dari tabel yang digunakan
+    protected $primaryKey = 'user_id';
 }
