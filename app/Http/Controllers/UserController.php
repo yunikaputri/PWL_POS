b5974899eacb399e89b5df124de8df731af8ef17
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-09-20 08:03:41 +0700

    Jobsheet 4_Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,20 +28,39 @@ class UserController extends Controller
         //     'password' => Hash::make('12345'), // Hashing password sebelum disimpan
         // ];
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'), // Hashing password sebelum disimpan
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'), // Hashing password sebelum disimpan
+        // ];
 
-        // Menambahkan data baru menggunakan metode Eloquent create
-        UserModel::create($data);
+        // // Menambahkan data baru menggunakan metode Eloquent create
+        // UserModel::create($data);
         
-        // UserModel::insert($data); // Tambahkan data ke tabel m_user
+        // // UserModel::insert($data); // Tambahkan data ke tabel m_user
 
-        // Coba akses model UserModel
-        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        // // Coba akses model UserModel
+        // $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(4, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
