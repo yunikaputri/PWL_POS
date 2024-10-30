f5e613a49281c788bb6c3d082113fbf8f0911b26
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-10-30 23:32:23 +0700

    Jobsheet 10_Praktikum 1

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\UserModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        // Set validation
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'nama' => 'required',
+            'password' => 'required|min:5|confirmed',
+            'level_id' => 'required'
+        ]);
+
+        // If validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create user
+        $user = UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password),
+            'level_id' => $request->level_id,
+        ]);
+
+        // Return response JSON if user is created
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'user' => $user,
+            ], 201);
+        }
+
+        // Return JSON if insert process fails
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
+}
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Models;
 
@@ -6,47 +6,65 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel  extends Authenticatable
+// class UserModel  extends Authenticatable
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user';
+//     protected $primaryKey = 'user_id';
+    // protected $fillable =['level_id', 'username', 'nama', 'password', 'created_at', 'avatar', 'updated_at'];
+
+//     protected $hidden = ['password']; // jangan di tampilkan saat select
+
+//     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+    
+//     /**
+//      * Relasi ke tabel
+//      */
+//     public function level(): BelongsTo{
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+
+//     /**
+//      * Mendapatkan nama role
+//      */
+//     public function getRoleName(): string
+//     {
+//         return $this->level->level_nama;
+//     }
+
+//     /*
+//      * Cek apakah user memiliki role tertentu
+//      */
+//     public function hasRole($role): bool
+//     {
+//         return $this->level->level_kode == $role;
+//     }
+
+//     /*
+//      * Mendapatkan kode role
+//      */
+//     public function getRole()
+//     {
+//         return $this->level->level_kode;
+//     }
+// }
+
+class UserModel  extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable =['level_id', 'username', 'nama', 'password', 'created_at', 'avatar', 'updated_at'];
-
-    protected $hidden = ['password']; // jangan di tampilkan saat select
-
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
-    
-    /**
-     * Relasi ke tabel
-     */
-    public function level(): BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
-
-    /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string
-    {
-        return $this->level->level_nama;
-    }
-
-    /*
-     * Cek apakah user memiliki role tertentu
-     */
-    public function hasRole($role): bool
-    {
-        return $this->level->level_kode == $role;
-    }
-
-    /*
-     * Mendapatkan kode role
-     */
-    public function getRole()
-    {
-        return $this->level->level_kode;
-    }
 }
